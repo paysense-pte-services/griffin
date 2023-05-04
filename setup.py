4050d47da2c26043a2983ed644ce6de6ef870f84
Supritha Krishnappa <supritha.krishnappa@PFIN-BLR-Q66TN97GJ0.local>
2023-05-04 13:55:19 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='griffin',
+    version='1.0',
+    description='Secret Management Library',
+    url='https://github.com/paysense-pte-services/griffin',
+    author='Supritha Krishnappa, Ritik Jain',
+    author_email='supritha.krishnappa@payufin.com, ritik.jain@payufin.com',
+    packages=['griffin'],
+    zip_safe=False,
+    install_requires=['hvac==1.1.0']
+)
