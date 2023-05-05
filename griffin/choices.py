5b5ae7be79ebca7f35bdafffc277d04f8b6edaed
Supritha Krishnappa <supritha.krishnappa@PFIN-BLR-Q66TN97GJ0.local>
2023-05-05 13:45:14 +0530

    change entity name

--- griffin/choices.py
+++ griffin/choices.py
@@ -3,6 +3,6 @@ from enum import Enum
 
 class EntityType(Enum):
     PAYUFIN = "payufin"
-    PAYSENSE = "paysense"
+    PAYSENSE = "paysense-pte"
     LAZYPAY = "lazypay"
     LAZYCARD = "lazycard"
