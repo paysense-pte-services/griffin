f70c9b1b860a6750f1cc649304115162b896302d
Supritha Krishnappa <supritha.krishnappa@PFIN-BLR-Q66TN97GJ0.local>
2023-05-09 13:18:55 +0530

    rollback

--- griffin/config.py
+++ griffin/config.py
@@ -6,7 +6,7 @@ ENTITY_NAME = os.environ.get("ENTITY_NAME")
 # env can be prod/sbox
 ENV = os.environ.get("ENV", "sbox")
 REGION = os.environ.get("REGION", "ap-south-1")
-SERVICE_NAME = "payment"
+SERVICE_NAME = os.environ.get("SERVICE_NAME")
 KEY = "current"
 MOUNT_POINT = "payu"
 # aws instance ec2
