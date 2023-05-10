f66f082616c291308221c5952670a7ca90f4a3fb
mohitkaushal50 <49854549+mohitkaushal50@users.noreply.github.com>
2023-05-10 20:19:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='griffin',
-    version='1.4',
+    version='1.5',
     description='Secret Management Library',
     url='https://github.com/paysense-pte-services/griffin',
     author='Supritha Krishnappa, Ritik Jain',
